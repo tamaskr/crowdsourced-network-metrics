237d7cd4cf65533e040e90e778028628180edfb1
András Ádám <adm.andras@gmail.com>
2022-10-27 14:17:35 +0300

    Set Europe as region for functions

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions'
 
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.region('europe-west1').https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true })
   response.send('Hello from Firebase!')
 })
