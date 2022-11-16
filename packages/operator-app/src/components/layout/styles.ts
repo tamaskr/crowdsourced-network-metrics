7491af8157e28c84be969ced58f2f468ac370464
Tamas Kralusz <tamas.kralusz@reactron.dev>
2022-11-16 21:06:51 +0200

    set up operator app layout + nav

---
+++ packages/operator-app/src/components/layout/styles.ts
@@ -0,0 +1,23 @@
+import { Grid, styled } from '@mui/material'
+import { Box } from '@mui/system'
+
+
+const Wrapper = styled(Box)(({ theme }) => ({
+  minHeight: '100vh',
+  backgroundColor: theme.palette.secondary.light
+}))
+
+const Container = styled(Grid)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center'
+})
+
+const ContentContainer = styled(Grid)(({ theme }) => ({
+  width: '100%',
+  maxWidth: '1920px',
+  padding: theme.spacing(3)
+}))
+
+
+export { Wrapper, Container, ContentContainer }
