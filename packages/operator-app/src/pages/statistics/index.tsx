59dd7d94d0899ee99e58977e07114d0aff41744e
Tamas Kralusz <tamas.kralusz@reactron.dev>
2022-11-23 20:52:41 +0200

    add statistics page UI with chart

---
+++ packages/operator-app/src/pages/statistics/index.tsx
@@ -0,0 +1,49 @@
+import { CircularProgress } from '@mui/material'
+import { useQuery } from '@tanstack/react-query'
+import { NextPage } from 'next'
+import { useMemo, useState } from 'react'
+import { toast } from 'react-toastify'
+import { Layout } from '../../components/layout'
+import { LoadingWrapper } from '../../theme/common'
+import { formatChartData } from '../../utils/chart'
+import { getMeasurements } from '../../services/queries'
+import { FormattedChartData } from '../../types/chart'
+import { StatisticsChart } from '../../components/charts/statistics'
+
+
+const Statistics: NextPage = () => {
+  const [ days, setDays ] = useState<number>(7)
+  const { isLoading, data } = useQuery(
+    [ '/measurements' ],
+    () =>
+      getMeasurements().catch(() =>
+        toast.error('Error while fetching measurement data')),
+    {
+      cacheTime: 0,
+      refetchOnWindowFocus: false
+    }
+  )
+
+  const chartData: FormattedChartData[] = useMemo(() => {
+    if (!data?.data) return []
+    return formatChartData(data.data, days)
+  }, [ data, days ])
+
+  return (
+    <Layout>
+      {isLoading ? (
+        <LoadingWrapper>
+          <CircularProgress />
+        </LoadingWrapper>
+      ) : (
+        <StatisticsChart
+          chartData={chartData}
+          days={days}
+          onDaysChange={event => setDays(Number(event.target.value))}
+        />
+      )}
+    </Layout>
+  )
+}
+
+export default Statistics
---
+++ packages/operator-app/src/theme/common.ts
@@ -0,0 +1,14 @@
+import { styled } from '@mui/material'
+import { Box } from '@mui/system'
+
+
+const LoadingWrapper = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  width: '100%',
+  alignItems: 'center',
+  justifyContent: 'center',
+  paddingTop: theme.spacing(6)
+}))
+
+
+export { LoadingWrapper }
