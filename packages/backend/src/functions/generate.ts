30bf30a58fcc641f336753bea3b51c2bc87fe8d9
András Ádám <adm.andras@gmail.com>
2022-12-02 13:07:29 +0200

    Update generate function to generate queries too

--- packages/backend/src/functions/generate.ts
+++ packages/backend/src/functions/generate.ts
@@ -1,44 +1,52 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
-import { MEASUREMENT_COLLECTION, REGION } from '../utils/constants'
+import { z } from 'zod'
+import { MEASUREMENT_COLLECTION, QUERY_COLLECTION, REGION } from '../utils/constants'
 import { cors } from '../utils/cors'
-import { generateMeasurement } from '../utils/random'
-import { Measurement } from '../types/types'
+import { generateMockData } from '../utils/random'
 
 
-// function to add documents to collection in firebase db
-const setDatabase = (list: Measurement[]): boolean => {
+// Generate a mock query with the provided number of measurements
+export const generate = functions.region(REGION).https.onRequest(async (request, response) => {
   try {
-    const docRef = admin.firestore().collection(MEASUREMENT_COLLECTION)
-    list.map(async (item: Measurement) => {
-      await docRef.doc(item.id).set(item)
-    })
-    return true
-  } catch (error) {
-    functions.logger.error(error, { structuredData: true })
-    return false
-  }
-}
+    // Handle CORS
+    const isPreflight = cors(request, response)
+    if (isPreflight) return
 
-// https request with the query parameter of the desired number of generated documents
-export const generate = functions.region(REGION).https.onRequest((request, response) => {
-  // Handle CORS
-  const isPreflight = cors(request, response)
-  if (isPreflight) return
-  // setting 10 as default amount of added documents
-  let count = 10
-  if (request.query && request.query.count) {
-    count  = +request.query.count
+    // Allow only GET requests to pass further
+    if (request.method !== 'GET') {
+      response.status(405).header('Allow', 'GET').json({ success: false })
+      return
+    }
+
+    // Validate the request query
+    const schema = z.object({
+      count: z.string().default('10').transform(Number).refine(val => Number.isSafeInteger(val) && val <= 500, {
+        message: 'Count is not a valid integer below 500.'
+      })
+    }).strict()
+    const result = schema.safeParse(request.query)
+    if (!result.success) {
+      response.status(400).json({ success: false, errors: result.error.errors })
+      return
+    }
+
+    // Generate mock query and measurements
+    const [ query, measurements ] = generateMockData(result.data.count)
+
+    // Save the query to the database
+    await admin.firestore().collection(QUERY_COLLECTION).doc(query.id).set(query)
+
+    // Save the measurements to the database, using a batch write for performance reasons
+    const collection = admin.firestore().collection(MEASUREMENT_COLLECTION)
+    const batchWrite = admin.firestore().batch()
+    measurements.forEach(measurement => batchWrite.set(collection.doc(measurement.id), measurement))
+    await batchWrite.commit()
+
+    // Respond with success true
+    response.status(200).json({ success: true })
+  } catch (error) {
+    functions.logger.error(error)
+    response.status(500).json({ success: false })
   }
-  // safe limit to add max 500 per call (might be deleted)
-  if (count > 500) {
-    count = 500
-  }
-  const list: Measurement[] = generateMeasurement(count)
-  const status: boolean = setDatabase(list)
-  if (!status) {
-    response.status(500).send('error 500')
-  }
-  functions.logger.info(`added ${count} objects`, { structuredData: true })
-  response.send('Ok')
 })
--- packages/backend/src/utils/random.ts
+++ packages/backend/src/utils/random.ts
@@ -1,28 +1,44 @@
 import { faker } from '@faker-js/faker'
 import * as uuid from 'uuid'
-import { Measurement } from '../types/types'
+import { Measurement, MeasurementType, Query } from '../types/types'
 
 
-// function that generate fake measurements
-export const generateMeasurement = (count: number): Measurement[] => {
+// Generate mock query and measurements
+export const generateMockData = (count: number): [ Query, Measurement[] ] => {
+  // Generate timestamp
   const now = Date.now()
   const weekAgo = Date.now() - 6.048e+8
-  const query = uuid.v4()
+  const timestamp = faker.date.between(weekAgo, now).valueOf()
 
-  const list: Measurement[] = []
+  // Generate coordinates
+  const coordinates = {
+    latitude: Number(faker.address.latitude(60.3, 60.15, 6)),
+    longitude: Number(faker.address.longitude(25.2, 24.6, 6))
+  }
+
+  // Generate query
+  const query: Query = {
+    id: uuid.v4(),
+    timestamp,
+    measurements: [ MeasurementType.Bandwidth, MeasurementType.Latency, MeasurementType.SignalStrength ],
+    coordinates,
+    range: 0.01,
+    responseCount: count
+  }
+
+  // Generate measurements
+  const measurements: Measurement[] = []
   for (let i = 0; i < count; i++) {
-    list.push({
+    measurements.push({
       id: uuid.v4(),
-      queryId: query,
-      timestamp: faker.date.between(weekAgo, now).valueOf(),
-      coordinates: {
-        latitude: Number(faker.address.latitude(60.3, 60.15, 6)),
-        longitude: Number(faker.address.longitude(25.2, 24.6, 6))
-      },
+      queryId: query.id,
+      timestamp,
+      coordinates,
       bandwidth: faker.datatype.number(faker.datatype.number({ min: 0, max: 50000 })),
       latency: faker.datatype.number(faker.datatype.number({ min: 0, max: 100 })),
       signalStrength: faker.datatype.number({ min: 0, max: 4 })
     })
   }
-  return list
+
+  return [ query, measurements ]
 }
