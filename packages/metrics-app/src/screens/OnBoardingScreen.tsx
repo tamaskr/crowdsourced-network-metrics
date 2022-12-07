e5f3b13065454cab7cba021cadd71b1f87dc071e
Ermiyas Assegu <ermiyasassegu@gmail.com>
2022-12-07 14:03:45 +0200

    add carousel improved tutorial

---
+++ packages/metrics-app/src/screens/OnBoardingScreen.tsx
@@ -0,0 +1,133 @@
+import React, { useRef, useState } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native'
+import { carouselItems } from '../utils/tutorialText'
+
+
+const { width, height } = Dimensions.get('screen')
+const PAGINATION_DOT_SIZE = 14
+const PAGINATION_DOT_UNSELECTED_COLOR = 'rgba(105, 105,105, 0.5)'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  contentContainer: {
+    width,
+    height,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontWeight: '800',
+    fontSize: 30,
+    marginBottom: 10,
+    color: '#493d8a',
+    textAlign: 'center'
+  },
+  description: {
+    fontWeight: '500',
+    color: '#62656b',
+    textAlign: 'center',
+    paddingHorizontal: 64
+  },
+  dotContainer: {
+    width,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  paginationDot: {
+    borderRadius: PAGINATION_DOT_SIZE,
+    width: PAGINATION_DOT_SIZE,
+    height: PAGINATION_DOT_SIZE,
+    marginBottom: 70,
+    margin: 10,
+    backgroundColor: PAGINATION_DOT_UNSELECTED_COLOR
+  },
+  paginationDotSelected: {
+    backgroundColor: 'black'
+  },
+  startButton: {
+    padding: 15,
+    backgroundColor: '#1160f2',
+    justifyContent: 'center',
+    borderRadius: 25,
+    margin: 40,
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  startText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18
+  }
+})
+
+interface CarouselItems {
+  title: string
+  description: string
+}
+export default function OnBoardingScreen() {
+  const flatListRef = useRef<FlatList<CarouselItems> | null>()
+  const [ currentIndex, setCurrentIndex ] = useState(0)
+
+  const onViewRef = useRef(({ changed }: { changed: any }) => {
+    if (changed[0].isViewable) {
+      setCurrentIndex(changed[0].index)
+    }
+  })
+  const scrollToIndex = (index: number) => {
+    flatListRef.current?.scrollToIndex({ animated: true, index: index })
+  }
+
+  const renderItems: React.FC<{ item: CarouselItems }> = ({ item }) => {
+    return (
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.description}>{item.description}</Text>
+      </View>
+    )
+  }
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={carouselItems}
+        renderItem={renderItems}
+        keyExtractor={(_, index) => index.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        ref={ref => {
+          flatListRef.current = ref
+        }}
+        onViewableItemsChanged={onViewRef.current}
+      />
+
+      <View style={styles.dotContainer}>
+        {carouselItems.map((_, index: number) => (
+          <TouchableOpacity
+            key={index.toString()}
+            style={[
+              styles.paginationDot,
+              currentIndex === index ? styles.paginationDotSelected : {}
+            ]}
+            onPress={() => scrollToIndex(index)}
+          />
+        ))}
+      </View>
+      <View>
+        <TouchableOpacity style={styles.startButton}>
+          <Text style={styles.startText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
--- packages/metrics-app/src/utils/tutorialText.ts
+++ packages/metrics-app/src/utils/tutorialText.ts
@@ -1,1 +1,20 @@
-export const text = 'We are going to collect your most private and personal data for manipulative use in the dark web, also the application will most likely consume a huge amount of your phones battery and slow down the processor a bit'
+export const text
+  = 'We are going to collect your most private and personal data for manipulative use in the dark web, also the application will most likely consume a huge amount of your phones battery and slow down the processor a bit'
+
+export const carouselItems = [
+  {
+    title: 'Welcome to Metrics App',
+    description:
+      'Please read appilcation review, permissions and settings before proceed'
+  },
+  {
+    title: 'Application Review',
+    description:
+      ' This application will help your mobile network provider understand your end-user experience by periodically collecting network quality metrics, such as signal strength, bandwidth and latency. You can opt out at any time, and no metric will be collected until you opt in again.'
+  },
+  {
+    title: 'Permissions and settings',
+    description:
+      'The app will require you to grant permissions for making phone calls to measure the signal strength, and precise location to map your network information to a geographic area. For the app to work in the background you also need to set location access as "all the time" in the settings.'
+  }
+]
