fce2054ec21e9c2ec915ba0d1d85c4e756a2c202
András Ádám <adm.andras@gmail.com>
2022-12-11 19:03:22 +0200

    Clean up navigation

--- packages/metrics-app/src/navigation/Navigator.tsx
+++ packages/metrics-app/src/navigation/Navigator.tsx
@@ -21,83 +21,63 @@ const styles = StyleSheet.create({
   }
 })
 
-// Set up tab navigator for main app flow
-const Tab = createMaterialBottomTabNavigator()
-
 // Set up stack navigation to determine if tutorial screen should be shown
-const Stack = createStackNavigator<RootStackParamList>()
+const RootStack = createStackNavigator<RootStackParamList>()
 
-const MainStack = () => {
-  const { t } = useTranslation()
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      activeColor={colors.background.white}
-      barStyle={styles.navigator}
-    >
-      <Tab.Screen
-        name={t('bottomNav.home')}
-        component={HomeScreen}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons
-              name="home-variant-outline"
-              color={color}
-              size={26}
-            />
-          )
-        }}
-      />
-      <Tab.Screen
-        name={t('bottomNav.history')}
-        component={HistoryScreen}
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="history" color={color} size={26} />
-          )
-        }}
-      />
-    </Tab.Navigator>
-  )
-}
-
-const Navigator = () => {
-  const [ initialRoute, setInitialRoute ] = useState<
-    keyof RootStackParamList | undefined
-  >()
-
-  useEffect(() => {
-    const checkTutorialToken = async () => {
-      const hasSeenTutorial = await getUserHasSeenTutorial()
-      setInitialRoute(hasSeenTutorial ? 'Main' : 'Tutorial')
-    }
-    checkTutorialToken()
-    SplashScreen.hideAsync()
-  }, [])
-
-  if (!initialRoute) return null
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={initialRoute}>
-        <Stack.Screen
-          name="Main"
-          component={MainStack}
-          options={{ headerShown: false, gestureEnabled: false }}
-        />
-        <Stack.Screen
-          name="Tutorial"
-          component={TutorialScreen}
-          options={{ headerShown: false, gestureEnabled: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
-
-export default Navigator
+// Set up tab navigator for main app flow
+const MainTab = createMaterialBottomTabNavigator()
 
 export type RootStackParamList = {
   Tutorial: undefined
   Main: undefined
+}
+
+export default function Navigator() {
+  const { t } = useTranslation()
+  const [ initialRoute, setInitialRoute ] = useState<keyof RootStackParamList | undefined>()
+
+  // Check if the user has seen the tutorial, set the correct route and hide the splash screen
+  useEffect(() => {
+    getUserHasSeenTutorial().then(hasSeenTutorial => {
+      setInitialRoute(hasSeenTutorial ? 'Main' : 'Tutorial')
+      SplashScreen.hideAsync()
+    })
+  }, [])
+
+  if (!initialRoute) return null
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator initialRouteName={initialRoute}>
+        <RootStack.Screen name="Tutorial" component={TutorialScreen} options={{ headerShown: false, gestureEnabled: false }} />
+        <RootStack.Screen name="Main" options={{ headerShown: false, gestureEnabled: false }}>
+          {() => (
+            <MainTab.Navigator
+              initialRouteName="Home"
+              activeColor={colors.background.white}
+              barStyle={styles.navigator}
+            >
+              <MainTab.Screen
+                name={t('bottomNav.home')}
+                component={HomeScreen}
+                options={{
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name="home-variant-outline" color={color} size={26} />
+                  )
+                }}
+              />
+              <MainTab.Screen
+                name={t('bottomNav.history')}
+                component={HistoryScreen}
+                options={{
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name="history" color={color} size={26} />
+                  )
+                }}
+              />
+            </MainTab.Navigator>
+          )}
+        </RootStack.Screen>
+      </RootStack.Navigator>
+    </NavigationContainer>
+  )
 }
